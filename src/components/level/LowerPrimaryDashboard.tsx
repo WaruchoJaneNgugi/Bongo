d4a0752ecfab500646b192dd0d72f0b0f0568362
jane-spothub <techspothubke@gmail.com>
2026-04-11 10:23:25 +0300

    updated overall look

--- src/components/level/LowerPrimaryDashboard.tsx
+++ src/components/level/LowerPrimaryDashboard.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
 import {LowerPrimary} from "./LowerPrimary/components/LowerPrimary.tsx";
 
-export const LowerPrimaryDashboard: React.FC<{ initialSubject?: string }> = ({ initialSubject }) => {
-  return <LowerPrimary initialSubject={initialSubject} />;
-};
+export const LowerPrimaryDashboard = ({ initialSubject }: { initialSubject?: string }) => (
+  <LowerPrimary initialSubject={initialSubject} />
+);
 
